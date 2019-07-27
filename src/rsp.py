3514a26b2191a54bf06ee1262c895af8a17f9ef0
Florian Salihovic <florian.salihovic@me.com>
2019-07-27 21:24:35 +0200

    feat(rules): basic implementation of rules for rock scissors paper

--- src/rsp.py
+++ src/rsp.py
@@ -12,12 +12,42 @@ class Game:
 
 
 class Rules:
+    rules = {
+        'rock': 'scissors',
+        'scissors': 'paper',
+        'paper': 'rocks'
+    }
+
     """
     Rules define which move beats another.
     """
 
     def __init__(self):
         pass
+
+    def compare(self, move_1, move_2):
+        """
+        Simple comparison of two values. If the first value is a key, which
+        reference the value which equals move_2, the player making move_1
+        wins.
+        If the second value is the key, which
+        :param move_1: A move done by the first player of a game.
+        :type move_1: basestring
+        :param move_2: A move done by the second player of a game.
+        :type move_2: basestring
+        :returns: an integer value in the set of {-1, 0, 1}, as every
+        comparator does.
+        :rtype: int
+        """
+        assert move_1 in self.rules, "move_1 is not a valid value"
+        assert move_2 in self.rules, "move_2 is not a valid value"
+
+        if self.rules[move_1] == move_2:
+            return -1
+        elif self.rules[move_2] == move_1:
+            return 1
+        else:
+            return 0
 
 
 class Round:
