ccf4ef398511da6e2d39537bae27cbb0876eacc7
Florian Salihovic <florian.salihovic@me.com>
2019-07-27 21:36:51 +0200

    feat(players): implementing `StaticMovePlayer`

--- src/rsp.py
+++ src/rsp.py
@@ -39,6 +39,7 @@ class Rules:
         comparator does.
         :rtype: int
         """
+        # https://docs.python.org/3.6/reference/simple_stmts.html#the-assert-statement
         assert move_1 in self.rules, "move_1 is not a valid value"
         assert move_2 in self.rules, "move_2 is not a valid value"
 
@@ -65,9 +66,34 @@ class Player:
     A player can play one move per round.
     """
 
-    def __init__():
+    def __init__(self):
         pass
+
+    def play(self):
+        """
+        Providing a value which is evaluated in a game to determine a winner.
+        :return: A value from the set of {'rock', 'scissors', 'paper'}.
+        :rtype: basestring
+        """
+        # https://docs.python.org/2/library/exceptions.html#exceptions.NotImplementedError
+        raise NotImplementedError('implement this method in '
+                                  'an appropriate base class')
+
+
+class StaticMovePlayer(Player):
+    """
+    StaticMovePlayer instance will always return the same value, which is
+    passed to the instance when created.
+    """
+
+    def __init__(self, value='rock'):
+        super().__init__()
+        self.value = value
+
+    def play(self):
+        return self.value
 
 
 if __name__ == '__main__':
     print("let's play a game")
+    assert 'rock' is StaticMovePlayer('rock').play()
